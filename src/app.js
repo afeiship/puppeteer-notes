cda12569c60081cbe7c76a95460cc8b5a717ad95
afeiship <1290657123@qq.com>
2019-07-24 20:28:03 +0800

    add: fetch html

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const puppeteer = require("puppeteer");
 
 puppeteer.launch({ headless: false }).then(async browser => {
   var page = await browser.newPage();
-  await page.goto("https://juejin.im");
-
-  var login = await page.$(".login");
-  await login.click();
-  var loginPhoneOrEmail = await page.$("[name=loginPhoneOrEmail]");
-  await loginPhoneOrEmail.click();
-  await page.type("18516697699@163.com", { delay: 20 });
+  await page.setJavaScriptEnabled(true);
+  await page.goto("https://free-ss.site/");
+  await page.waitFor(5000);
+  const node = await page.evaluate(async () => {
+    return document.querySelector("#tbss").innerHTML;
+  });
+  console.log(node);
 });
