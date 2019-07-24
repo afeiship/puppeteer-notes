e08babda9d3335b2324f369f2e0df3183cfce932
afeiship <1290657123@qq.com>
2019-07-24 20:38:02 +0800

    add: updated ss account

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const puppeteer = require("puppeteer");
 
-puppeteer.launch({ headless: true }).then(async browser => {
+puppeteer.launch({ headless: false }).then(async browser => {
   var page = await browser.newPage();
   await page.setJavaScriptEnabled(true);
-  await page.goto("https://free-ss.site/");
-  await page.waitFor(5000);
+  await page.goto("https://free-ss.site/", { waitUntil: "networkidle0" });
   const node = await page.evaluate(async () => {
     return document.querySelector("#tbss").innerHTML;
   });
